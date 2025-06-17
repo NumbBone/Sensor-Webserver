35c2a9aceb4040132b173718e31a23ae4e1a45bb
Denis Miri <denismiri1234@gmail.com>
2025-06-17 14:07:26 +0200

    resized icons

--- frontend/src/scenes/global/Topbar.jsx
+++ frontend/src/scenes/global/Topbar.jsx
@@ -1,4 +1,4 @@
-import {Box, IconButton, useTheme} from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../themes";
 import InputBase from "@mui/material/InputBase";
@@ -9,34 +9,34 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 
-
-
 const Topbar = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
 
-
-    return (
-        <Box display="flex" justifyContent="space-between" p={2}>
-            <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
-            
-            <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-            <IconButton type ="button" sx={{ p: 1 }}>
-                <SearchIcon />
-            </IconButton>
-            </Box>
-            <Box display="flex">
-                <IconButton onClick={colorMode.toggleColorMode}>
-                    {theme.palette.mode === "dark" ? (
-                        <DarkModeOutlinedIcon />
-                    ) : (
-                        <LightModeOutlinedIcon />
-                    )}
-                </IconButton>
-            </Box>
-
-    </Box>);
-}
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon fontSize="large" />
+        </IconButton>
+      </Box>
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon fontSize="large" />
+          ) : (
+            <LightModeOutlinedIcon fontSize="large" />
+          )}
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 
 export default Topbar;
